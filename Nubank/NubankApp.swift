08fa8ecd7ea80c0757d9e08116471b544dd55385
LuizFranc0 <luizmichelfranco@hotmail.com>
2023-02-26 16:45:14 -0300

    Initial Commit

---
+++ Nubank/NubankApp.swift
@@ -0,0 +1,17 @@
+//
+//  NubankApp.swift
+//  Nubank
+//
+//  Created by Luiz Franco on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct NubankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

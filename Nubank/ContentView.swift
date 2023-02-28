a51b365892fd21130429aca868041c99c6b9e938
Luiz Franco <116929967+LuizFranc0@users.noreply.github.com>
2023-02-27 23:26:36 -0300

    Update ContentView.swift

--- Nubank/ContentView.swift
+++ Nubank/ContentView.swift
@@ -3,16 +3,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-//        .padding()
+        ScrollView(showsIndicators: false){
+            VStack{
+                HeaderView()
+                VStack{
+                    AcountView()
+                    
+                    CustomDivider
+                    CreditCardView()
+                    CustomDivider
+                    PaymentAssistantView()
+                    CustomDivider
+                    BorrowView()
+                } .background(Color.white)
+            }
+        } .background(
+            VStack{
+                nubankColor.ignoresSafeArea() ;Color.white
+            }
+        )
     }
 }
-
+extension ContentView {
+    var CustomDivider: some View {
+        Divider()
+            .frame(minHeight: 2)
+            .background(Color.gray.opacity(0.2))
+            .padding(.vertical)
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
